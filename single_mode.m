2a58658f0f352e8d78852e649a7a7f696df24abe
KODO <kdg930202@naver.com>
2024-11-21 20:40:04 +0900

    add

--- single_mode.m
+++ single_mode.m
@@ -13,19 +13,20 @@ alpha = s*sin(phi);
 a = abs(alpha)*exp(1i*theta);
 
 % Matrix exponential : expm
-S = expm(a*a1'*a1' - a'*a1*a1); 
+S = expm(0.5*(a'*a1*a1 - a*a1'*a1')); 
 
 %%
 n_bar = s^2 * (cos(phi))^2 ;
+% n_bar = 0.5;
 
 for i=1:d
-    rho_th(i) = (1/(1 + n_bar))^2 * (n_bar/(1 + n_bar))^(i-1);
+    rho_th(i) = (1/(1 + n_bar)) * (n_bar/(1 + n_bar))^(i-1);
     % Rho_th = Rho_th + rho_th * kron(I_a(:,i+1),I_a(:,i+1));
 end
 
 rho_th = diag(rho_th);
-trace(rho_th)
+[trace(rho_th),trace(rho_th*a1'*a1)] %should be 1 and n_bar
 
 rho_sq_th = S*rho_th*S';
 % rho_sq_th = S*rho_th*conj(S);
-trace(rho_sq_th)
+[trace(rho_sq_th),trace(rho_sq_th*a1'*a1)] %should be 1 and n_bar
