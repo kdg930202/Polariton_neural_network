5103c8cb5dd6f5cb04d483dc08a654e808db2d72
KODO <kdg930202@naver.com>
2025-01-03 14:15:40 +0100

    add

--- g2_comparison.m
+++ g2_comparison.m
@@ -1,4 +1,4 @@
-clearvars
+function [g2_s_an, g2_s_nu] = g2_comparison(alpha, nth)
 
 p1 = 1;
 p2 = 2;
@@ -7,12 +7,12 @@ denom = sqrt(p1^2 + p2^2 + p3^2);
 p1 = p1/denom;
 p2 = p2/denom;
 p3 = p3/denom;
-ps_sum = p1^2 + p2^2 + p3^2;
+% ps_sum = p1^2 + p2^2 + p3^2;
 d = 10; %dimension of the annihilation and creation operator
 a = diag(sqrt(1:d-1),1); %annihilation operator
 p_number = 7;
 I = eye(d);
-alpha = 0.5;
+% alpha = 0.1;
 
 %%
 % Coherent state
@@ -24,18 +24,18 @@ for i = 0:d-1
     end
 end
 
-trace(rho_coh)
+% trace(rho_coh)
 g2_coh = trace(a'*a'*a*a*rho_coh)/trace(a'*a*rho_coh)^2; %should be 2
 
 %% 
 % Thermal state
-nth = 0.6;
+% nth = 0.5;
 rho_th = 0;
 
 for i=0:d-1
     rho_th = rho_th+(nth^i/(1+nth)^(i+1))*I(:,i+1)*I(:,i+1)'; 
 end
-trace(rho_th)
+% trace(rho_th)
 g2_th = trace(a'*a'*a*a*rho_th)/trace(a'*a*rho_th)^2; %should be 1
 
 %%
@@ -51,3 +51,5 @@ rho_s = p1*rho_fock + p2*rho_coh + p3*rho_th;
 
 g2_s_an = (p1*p_number*(p_number-1) + p2*alpha^4 + 2*p3*nth^2)/(p1*p_number + p2*alpha^2 + p3*nth)^2;
 g2_s_nu = trace(a'*a'*a*a*rho_s)/trace(a'*a*rho_s)^2;
+
+end
---
+++ g2_comparison_data_geneartion.m
@@ -0,0 +1,29 @@
+clearvars
+nth = 0:0.01:1;
+alpha = 0:0.01:1;
+
+result_an = zeros(length(nth), length(alpha));
+result_nu = zeros(length(nth), length(alpha));
+
+for i = 1:length(nth)
+    for j = 1:length(alpha)
+
+        [result_an(i,j), result_nu(i,j)] = g2_comparison(nth(i), alpha(j));
+    end
+end
+
+%%
+
+figure()
+subplot(1,2,1)
+pcolor(nth, alpha,result_an)
+xlabel("nth")
+ylabel("alpha")
+title("Analytics")
+colorbar()
+subplot(1,2,2)
+pcolor(nth, alpha,result_nu)
+colorbar()
+xlabel("nth")
+ylabel("alpha")
+title("Numerics")
