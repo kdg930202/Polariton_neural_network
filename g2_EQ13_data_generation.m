b01c36b373cdc8a4a4077e83256c7167566b51ed
KODO <kdg930202@naver.com>
2025-01-15 12:16:13 +0100

    addd

--- g2_EQ13_data_generation.m
+++ g2_EQ13_data_generation.m
@@ -2,16 +2,19 @@ clearvars
 close all
 
 theta = 0:0.1:pi;
-phi = 0:0.1:2*pi;
+phi = 0:0.1:pi;
 
 
 g2 = NaN(length(phi), length(theta));
 % result_nu = zeros(length(phi), length(theta));
+n1_seleted = cell(length(phi), length(theta));
+n2_seleted = cell(length(phi), length(theta));
+
 
 for i = 1:length(phi)
     for j = 1:length(theta)
 
-        [g2(i,j), n1_seleted, n2_selected] = g2_EQ13(phi(i), theta(j));
+        [g2(i,j), n1_selected{i,j}, n2_selected{i,j}] = g2_EQ13(phi(i), theta(j));
         % g2(i,j) = g2_comparison_new_method(phi(i), theta(j));
 
     end
