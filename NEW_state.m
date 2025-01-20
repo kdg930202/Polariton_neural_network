9737033a381c1ccb1f52441f18209c22813c0fda
KODO <kdg930202@naver.com>
2025-01-20 13:01:59 +0100

    add

--- NEW_state.m
+++ NEW_state.m
@@ -10,13 +10,14 @@ I_a = eye(d);
 a1 = aa;
 phi = 0.5+pi/10;
 alpha = s*sin(phi);
-r = abs(alpha)*exp(1i*theta);
+% r = abs(alpha)*exp(1i*theta);
+r = 2;
 
 % Matrix exponential : expm
-S = expm(0.5*(r'*a1*a1/2 - r*a1'*a1'/2)); 
+S = expm(0.5*(r'*a1*a1 - r*a1'*a1')); 
 
 adm = I_a;
-m = 2;
+m = 0;
 for i=1:m
     adm = a1'*adm;
 end
@@ -25,26 +26,27 @@ psi_1 = adm*S*vacc;
 
 %%
 clc
-m=0;
-ns = 0:2:d;
+% m=0;
 % I_a(:,m+1)
 
 Bm_sum = 0;
 for n=1:d
-    if 2*(n-1)+m>d
+    if 2*(n-1)+m>=d
         break
     end
-    2*(n-1)+m  
-    Bm_sum = Bm_sum + factorial(2*(n-1)+m)/(factorial(n-1)^2);
+    % 2*(n-1)+m  
+    Bm_sum = Bm_sum + (r/2)^4*factorial(2*(n-1)+m)/(factorial(n-1)^2);
+    [2*(n-1)+m,factorial(2*(n-1)+m)/(factorial(n-1)^2)]
 end
 Nm = 1/sqrt(Bm_sum);
 psi = zeros(1,d)';
 for n=1:d
-    if 2*(n-1)+m+1>d
+    if 2*(n-1)+m+1>=d
         break
     end
     % 2*(n-1)+m
-    Bm = (r/2)^2*factorial(2*(n-1)+m)/(factorial(n-1)^2);
+    Bm = (r/2)^2*sqrt(factorial(2*(n-1)+m))/(factorial(n-1));
     psi = psi + Nm*Bm*I_a(:,2*(n-1)+m+1);
+    % [2*(n-1)+m,2*(n-1)+m+1,Bm]
 end
 %%
